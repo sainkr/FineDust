c95a9b38d5f420178251450494726a7f47febfea
sainkr <raebadnap@naver.com>
2021-05-25 23:24:02 +0900

    Initial Commit

---
+++ FineDust/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FineDust
+//
+//  Created by 홍승아 on 2021/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
